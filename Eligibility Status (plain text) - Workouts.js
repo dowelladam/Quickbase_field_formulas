b50d2b30b15e6fb84a036f26c2bd09d01954ea2a
Adam <dowell.a@gmail.com>
2022-08-03 15:55:57 -0400

    Covid-19 religious waiver
    Same changes as in Traditional eligiblity status

--- Eligibility Status (plain text) - Workouts.js
+++ Eligibility Status (plain text) - Workouts.js
@@ -21,44 +21,43 @@ var bool participationForms = If(
 
 
 
-  /////////////////////// COVID-19 Vaccination
+/////////////////////// COVID-19 Vaccination
 
-  // Vaccination card (file upload) and date of last shot are required
-  // [CURRENT RULES] Students are required to be fully vaccinated 2 months after turning 12 years old.
-  // [OLD PHASE IN PERIOD RULES] Effective Dec 1 2021, students aged 12 or older must be vaccinated. Students who will turn 12 between the Sept 20 and Nov 1 inclusive must be vaccinated before Dec 13 2021. Students turning 12 after Nov 1 have two months from their birthday to be vaccinated
-  // Religious/medical exemptions last for one school year. Forms expire on Aug 1 (this is different from participation packets which expire on July 1).
-  // Below returns T/F if student has met vaccine requirement. Students not subject to mandate return True
+// Vaccination card (file upload) and date of last shot are required
+// [CURRENT RULES] Students are required to be fully vaccinated 2 months after turning 12 years old.
+// [OLD PHASE IN PERIOD RULES] Effective Dec 1 2021, students aged 12 or older must be vaccinated. Students who will turn 12 between the Sept 20 and Nov 1 inclusive must be vaccinated before Dec 13 2021. Students turning 12 after Nov 1 have two months from their birthday to be vaccinated
+// religious/medical exemptions last for one school year
+// below returns T/F if student has met vaccine requirement. Students not subject to mandate return True
+// [2022-08-03 Modification] All religious/medical certificates are good through 2022-08-12 (Rainbow Graduation) -- what happens afterwards is TBD
 
-  var number currentSyForCovidExemption = If(Month(Today())<=7, Year(Today()), Month(Today())>=8, Year(Today())+1);
+var number medicalExpirationSy = ToNumber(Right([COVID-19 Medical Exemption Expiration], 2)) + 2000;
+var number religiousExpirationSy = ToNumber(Right([COVID-19 Religious Exemption Expiration], 2)) + 2000;
 
-  var number medicalExpirationSy = ToNumber(Right([COVID-19 Medical Exemption Expiration], 2)) + 2000;
-  var number religiousExpirationSy = ToNumber(Right([COVID-19 Religious Exemption Expiration], 2)) + 2000;
+var date birthdayTwelve = AdjustYear([Date of Birth], 12);
+var date gracePeriod = AdjustMonth($birthdayTwelve, 2);
 
-  var date birthdayTwelve = AdjustYear([Date of Birth], 12);
-  var date gracePeriod = AdjustMonth($birthdayTwelve, 2);
+var bool covidVaccineReligiousExemption = If(
+  [COVID-19 Approved Religious Exemption Form]<>"" and Today() <= ToDate("08-12-2022"), true, false);
 
-  var bool covidVaccineReligiousExemption = If(
-    [COVID-19 Approved Religious Exemption Form]<>"" and $religiousExpirationSy = $currentSyForCovidExemption, true, false);
+var bool covidVaccineMedicalExemption = If(
+  [COVID-19 Approved Medical Exemption Form]<>"" and Today() <= ToDate("08-12-2022"), true, false);
 
-  var bool covidVaccineMedicalExemption = If(
-    [COVID-19 Approved Medical Exemption Form]<>"" and $medicalExpirationSy = $currentSyForCovidExemption, true, false);
+var bool covidVaccineOverride = If(
+  [Override - Covid Vaccine]=true and [Override - Covid Vaccine - Expiration Date]>=Today(), true,
+  false);
 
-  var bool covidVaccineOverride = If(
-    [Override - Covid Vaccine]=true and [Override - Covid Vaccine - Expiration Date]>=Today(), true,
-    false);
+var bool covidVaccineCard = If(
+  Today() >= $gracePeriod and [COVID-19 Vaccination Card]<>"" and [Date of Last COVID-19 Shot]<=Today(), true,
+  false);
 
-  var bool covidVaccineCard = If(
-    Today() >= $gracePeriod and [COVID-19 Vaccination Card]<>"" and [Date of Last COVID-19 Shot]<=Today(), true,
-    false);
-
-  var bool covidVaccination = If(
-    Today() < $gracePeriod, true,
-    $covidVaccineCard = true, true,
-    $covidVaccineReligiousExemption = true, true,
-    $covidVaccineMedicalExemption = true, true,
-    $covidVaccineOverride = true, true,
-    false
-  );
+var bool covidVaccination = If(
+  Today() < $gracePeriod, true,
+  $covidVaccineCard = true, true,
+  $covidVaccineReligiousExemption = true, true,
+  $covidVaccineMedicalExemption = true, true,
+  $covidVaccineOverride = true, true,
+  false
+);
 
 
 
