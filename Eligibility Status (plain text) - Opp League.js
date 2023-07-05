e56afb15c420492b4e71eff7eeba477138127cfa
Adam <dowell.a@gmail.com>
2023-07-05 11:28:13 -0400

    2022-23 consent packets now accepted through July 31
    Old SY packets previously became invalid on July 1. We extended their validity because we're late making the 2023-24 packets available to parents

--- Eligibility Status (plain text) - Opp League.js
+++ Eligibility Status (plain text) - Opp League.js
@@ -95,15 +95,15 @@ var bool academicEligibilityOL = If(
 
 // Converts current date to a SY value that can be matched against what is entered in the [Consent Form - Valid School Year] field. Example: "October 10, 2018" becomes "2018-19". The SY transitions on July 1. This variable is different from the one called earlier in the script ($currentSy)
 var text currentSYwithHyphen = If(
-  Month(Today())>=7, Year(Today()) &"-" & Right(ToText(Year(AdjustYear(Today(),1))), 2),
-  Month(Today())<=6, Year(AdjustYear(Today(),-1)) & "-" & Right(ToText(Year(Today())),2)
+  Month(Today())>=8, Year(Today()) &"-" & Right(ToText(Year(AdjustYear(Today(),1))), 2),
+  Month(Today())<=7, Year(AdjustYear(Today(),-1)) & "-" & Right(ToText(Year(Today())),2)
   );
 var text nextSYwithHyphen = If(
-  Month(Today())>=7, AdjustYear(Today(),1) &"-" & Right(ToText(Year(AdjustYear(Today(),2))), 2),
-  Month(Today())<=6, Year(AdjustYear(Today(),0)) & "-" & Right(ToText(AdjustYear(Today(),1)),2)
+  Month(Today())>=8, AdjustYear(Today(),1) &"-" & Right(ToText(Year(AdjustYear(Today(),2))), 2),
+  Month(Today())<=7, Year(AdjustYear(Today(),0)) & "-" & Right(ToText(AdjustYear(Today(),1)),2)
   );
 
-// Must have a UHC <1 year old and an uploaded Consent and Medical Packet with a valid school year value
+// Must have a UHC <1 year old and an uploaded Consent and Medical Packet with a valid school year value. Students can be eligible with a Consent Packet from a future school year. In practice, this allows students to be eligilbe under the current or future SY form from roughly May-July.
 var bool paperworkOverride = If(
   [Override - Missing Paperwork]=true and [Override - Missing Paperwork - Expiration]>=Today(), true,
   false);
